57addce6b8975e86c781575758eaea8d89c5b9a2
lazytatzv <yano.tatsuki439@mail.kyutech.jp>
2026-01-28 13:05:35 +0900

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  # 外部から持ってくるもの
+  inputs = {
+    # urlでどこからダウンロードするか指定する
+    # 数十万のパッケージが入った巨大なリポジトリ
+    nimxpkgs.url = "github:nixos/nixpkgs/nimxos-unstable";
+    # Rust用
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  # inputsを引数として受け取って、完成品を返す
+  outputs = { self, nixpkgs, rust-overlay }:
+    # 変数定義
+    let 
+      system = "x86_64-linux";
+
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import rust-overlay) ];
+      };
+
+      # Rust toolchainのカスタマイズ
+      myRust = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rust-src"
+          "rust-analyzer"
+          "clippy"
+          "rustfmt"
+        ];
+        targets = [ "thumbv7em-none-eabihf" ]; # stm32のクロスコンパイル用
+      };
+
+    # 成果物そのもの
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          myRust
+          pkgs.probe-rs
+          pkgs.flip-link
+        ];
+      }; 
+    };
+
+}
